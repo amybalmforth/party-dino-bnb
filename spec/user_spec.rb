a7017fd250629527050ca4bb827d99f407c314b6
Student <student@Makerss-MacBook-Pro.local>
2019-11-18 15:41:23 +0000

    users class creates a new user in the users db

---
+++ spec/user_spec.rb
@@ -0,0 +1,12 @@
+require 'user'
+
+describe "User" do
+
+  it "creates a new user" do
+    user1 = Users.create(name: "John Smith", username: "johnsmith", email: "jsmith@gmail.com", password: "whatever")
+    user2 = Users.create(name: "Jane Doe", username: "janedoe", email: "jdoe@gmail.com", password: "whatever2")
+
+    expect(user1.username).to eq("johnsmith")
+    expect(user2.password).to eq("whatever2")
+  end
+end
