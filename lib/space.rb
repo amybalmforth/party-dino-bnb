25fa3bd0fd3e9c4fb214ad36add699962537c482
Student <student@Makerss-MacBook-Pro.local>
2019-11-18 14:13:57 +0000

    returns a list of spaces, 2 tests 1 pass

--- lib/space.rb
+++ lib/space.rb
@@ -1,6 +1,6 @@
 require 'pg'
 
-class Space
+class Spaces
 
   def initialize(id:, name:, description:, price:, available_from:, available_to:)
 
@@ -14,7 +14,20 @@ class Space
     end
 
     result = conn.exec("INSERT INTO spaces (name, description, price, available_from, available_to) VALUES('#{name}', '#{description}', '#{price}', '#{available_from}', '#{available_to}') RETURNING name, description, price, available_from, available_to")
-    Space.new(id: result[0]['id'], name: result[0]["name"], description: result[0]["description"], price: result[0]["price"], available_from: result[0]["available_from"], available_to: result[0]["available_to"])
+    Spaces.new(id: result[0]['id'], name: result[0]["name"], description: result[0]["description"], price: result[0]["price"], available_from: result[0]["available_from"], available_to: result[0]["available_to"])
+  end
+
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      conn = PG.connect(dbname: 'party_dino_bnb_test')
+    else
+      conn = PG.connect(dbname: 'party_dino_bnb')
+    end
+
+    result = conn.exec("SELECT * FROM spaces")
+    result.map do |row|
+      Spaces.new(id: row["id"], name: row["name"], description: row["description"], price: row["price"], available_from: row["available_from"], available_to: row["available_to"])
+    end
   end
 
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,14 +1,14 @@
-feature 'Viewing Spaces' do
+describe 'Viewing Spaces' do
 
-  scenario 'user can see spaces' do
+  it 'returns a list of spaces' do
     conn = PG.connect(dbname: 'party_dino_bnb_test')
 
     Spaces.create(name: 'home', description: 'urban area', price: '£50', available_from: 'November 18 2019', available_to: 'November 25 2019')
     Spaces.create(name: 'house', description: 'rural area', price: '£40', available_from: 'November 20 2019', available_to: 'November 30 2019')
 
-    visit('/spaces')
+    spaces = Spaces.all
 
-    expect(page).to have_content "home" && "urban area" && "£50" && "November 25 2019" && "November 18 2019"
-    expect(page).to have_content "house" && "rural area" && "£40" && "November 20 2019" && "November 30 2019"
+    expect(spaces.length). to eq 2
+    expect(spaces.first).to be_a Spaces
   end
 end
