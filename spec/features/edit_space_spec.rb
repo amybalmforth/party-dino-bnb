1e19f1bfa6db95f36b3eb5c8545d922f4047bf8f
ellieSMASH <eliseaston@gmail.com>
2019-11-21 14:06:41 +0000

    More test refactoring

--- spec/features/edit_space_spec.rb
+++ spec/features/edit_space_spec.rb
@@ -7,12 +7,7 @@ feature 'edit spaces' do
   end
 
   scenario 'user can edit a space' do
-    visit('/signup')
-    fill_in 'name', with: 'John Smith'
-    fill_in 'username', with: 'JohnSmith'
-    fill_in 'email', with: 'jsmith@gmail.com'
-    fill_in 'password', with: 'backseatfreestyle'
-    click_button('submit')
+    signup_and_in
     click_button 'Edit'
     expect(find_field('description').value).to eq 'urban area'
   end
